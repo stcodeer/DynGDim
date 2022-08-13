8418c263854068ed65032a5a67f2bcc5f54a3526
stcodeer <770780079@qq.com>
2022-08-13 17:26:23 +0800

    add directed graph support

--- dyngdim/__init__.py
+++ dyngdim/__init__.py
@@ -1,10 +1,10 @@
 """import the main function for direct use"""
 from .dyngdim import (
-    compute_global_dimension,
+    # compute_global_dimension,
     get_initial_measure,
     run_all_sources,
-    run_local_dimension,
-    run_local_dimension_from_sources,
+    # run_local_dimension,
+    # run_local_dimension_from_sources,
     run_several_sources,
-    run_single_source,
+    # run_single_source,
 )
--- dyngdim/dyngdim.py
+++ dyngdim/dyngdim.py
@@ -4,7 +4,7 @@ import warnings
 
 import networkx as nx
 import numpy as np
-from scipy.sparse.linalg import eigs, expm_multiply
+from scipy.sparse.linalg import eigs, expm_multiply, expm
 from scipy.sparse import diags
 from tqdm import tqdm
 
@@ -29,10 +29,13 @@ class Worker:
         )[:2]
 
 
-def run_all_sources(graph, times, use_spectral_gap=True, n_workers=1, disable_tqdm=False):
+def run_all_sources(graph, times, use_spectral_gap=True, n_workers=1, disable_tqdm=False, directed=False):
     """compute relative dimensions of all the nodes in a graph"""
-    total_degree = sum(graph.degree(u, weight="weight") for u in graph)
-    sources = [get_initial_measure(graph, [node], total_degree) for node in graph]
+    if directed:
+        total_degree = sum(graph.out_degree(u, weight="weight") for u in graph)
+    else:
+        total_degree = sum(graph.degree(u, weight="weight") for u in graph)
+    sources = [get_initial_measure(graph, [node], total_degree, directed) for node in graph]
     return run_several_sources(
         graph,
         times,
@@ -40,11 +43,12 @@ def run_all_sources(graph, times, use_spectral_gap=True, n_workers=1, disable_tq
         use_spectral_gap=use_spectral_gap,
         n_workers=n_workers,
         disable_tqdm=disable_tqdm,
+        directed=directed,
     )
 
 
 def run_several_sources(
-    graph, times, sources, use_spectral_gap=True, n_workers=1, disable_tqdm=False
+    graph, times, sources, use_spectral_gap=True, n_workers=1, disable_tqdm=False, directed=False
 ):
     """relative dimensions from a list of sources"""
     print("Construct Laplacian Begin.")
@@ -75,80 +79,84 @@ def run_several_sources(
     return relative_dimensions, peak_times
 
 
-def run_single_source(graph, times, initial_measure, use_spectral_gap=True):
-    """main function to compute relative dimensions"""
-    laplacian, spectral_gap = construct_laplacian(graph, use_spectral_gap=use_spectral_gap)
+# def run_single_source(graph, times, initial_measure, use_spectral_gap=True):
+#     """main function to compute relative dimensions"""
+#     laplacian, spectral_gap = construct_laplacian(graph, use_spectral_gap=use_spectral_gap)
 
-    node_trajectories = compute_node_trajectories(laplacian, initial_measure, times)
-    (
-        relative_dimensions,
-        peak_times,
-        peak_amplitudes,
-        diffusion_coefficient,
-    ) = extract_relative_dimensions(times, node_trajectories, initial_measure, spectral_gap)
+#     node_trajectories = compute_node_trajectories(laplacian, initial_measure, times)
+#     (
+#         relative_dimensions,
+#         peak_times,
+#         peak_amplitudes,
+#         diffusion_coefficient,
+#     ) = extract_relative_dimensions(times, node_trajectories, initial_measure, spectral_gap)
 
-    results = {
-        "relative_dimensions": relative_dimensions,
-        "peak_amplitudes": peak_amplitudes,
-        "peak_times": peak_times,
-        "diffusion_coefficient": diffusion_coefficient,
-        "times": times,
-        "node_trajectories": node_trajectories,
-    }
+#     results = {
+#         "relative_dimensions": relative_dimensions,
+#         "peak_amplitudes": peak_amplitudes,
+#         "peak_times": peak_times,
+#         "diffusion_coefficient": diffusion_coefficient,
+#         "times": times,
+#         "node_trajectories": node_trajectories,
+#     }
 
-    return results
-
-
-def run_local_dimension(
-    graph, times, use_spectral_gap=True, n_workers=1, nodes=None, disable_tqdm=False
-):
-    """computing the local dimensionality of each node"""
-    if nodes is None:
-        nodes = graph
-    sources = [get_initial_measure(graph, [node], sum(graph.degree(u, weight="weight") for u in graph)) for node in nodes]
-    return run_local_dimension_from_sources(
-        graph,
-        times,
-        sources,
-        use_spectral_gap=use_spectral_gap,
-        n_workers=n_workers,
-        disable_tqdm=disable_tqdm,
-    )
+#     return results
 
 
-def run_local_dimension_from_sources(
-    graph, times, sources, use_spectral_gap=True, n_workers=1, disable_tqdm=False
-):
-    """computing the local dimensionality of each source"""
-    relative_dimensions, peak_times = run_several_sources(
-        graph,
-        times,
-        sources,
-        use_spectral_gap,
-        n_workers,
-        disable_tqdm=disable_tqdm,
-    )
-
-    local_dimensions = []
-    for time_horizon in times:
-        relative_dimensions_reduced = relative_dimensions.copy()
-        relative_dimensions_reduced[peak_times > time_horizon] = np.nan
-        local_dimensions.append(np.nanmean(relative_dimensions_reduced, axis=1))
-
-    return np.array(local_dimensions)
+# def run_local_dimension(
+#     graph, times, use_spectral_gap=True, n_workers=1, nodes=None, disable_tqdm=False
+# ):
+#     """computing the local dimensionality of each node"""
+#     if nodes is None:
+#         nodes = graph
+#     total_degree = sum(graph.degree(u, weight="weight") for u in nodes)
+#     sources = [get_initial_measure(graph, [node], total_degree) for node in nodes]
+#     return run_local_dimension_from_sources(
+#         graph,
+#         times,
+#         sources,
+#         use_spectral_gap=use_spectral_gap,
+#         n_workers=n_workers,
+#         disable_tqdm=disable_tqdm,
+#     )
 
 
-def compute_global_dimension(local_dimensions):
-    """Computing the global dimensiona of the graph"""
-    return local_dimensions.mean(1)
+# def run_local_dimension_from_sources(
+#     graph, times, sources, use_spectral_gap=True, n_workers=1, disable_tqdm=False
+# ):
+#     """computing the local dimensionality of each source"""
+#     relative_dimensions, peak_times = run_several_sources(
+#         graph,
+#         times,
+#         sources,
+#         use_spectral_gap,
+#         n_workers,
+#         disable_tqdm=disable_tqdm,
+#     )
+
+#     local_dimensions = []
+#     for time_horizon in times:
+#         relative_dimensions_reduced = relative_dimensions.copy()
+#         relative_dimensions_reduced[peak_times > time_horizon] = np.nan
+#         local_dimensions.append(np.nanmean(relative_dimensions_reduced, axis=1))
+
+#     return np.array(local_dimensions)
 
 
-def construct_laplacian(graph, laplacian_tpe="normalized", use_spectral_gap=True):
+# def compute_global_dimension(local_dimensions):
+#     """Computing the global dimensiona of the graph"""
+#     return local_dimensions.mean(1)
+
+
+def construct_laplacian(graph, laplacian_tpe="normalized", use_spectral_gap=True, directed=False):
     """construct the Laplacian matrix"""
     if laplacian_tpe == "normalized":
-        degrees = np.array([graph.degree(i, weight="weight") for i in graph.nodes])
-        laplacian = diags(1.0 / degrees).dot(nx.laplacian_matrix(graph))
-        # laplacian = nx.laplacian_matrix(graph).dot(diags(1.0 / degrees))
+        if directed:
+            degrees = np.array([graph.out_degree(i, weight="weight") for i in graph.nodes], dtype = float)
+            laplacian = diags(1.0 / degrees).dot(nx.directed_laplacian_matrix(graph))
+        else:
+            degrees = np.array([graph.degree(i, weight="weight") for i in graph.nodes], dtype = float)
+            laplacian = diags(1.0 / degrees).dot(nx.laplacian_matrix(graph))
     else:
         raise Exception(
             "Any other laplacian type than normalized are not implemented as they will not work"
@@ -165,6 +173,7 @@ def construct_laplacian(graph, laplacian_tpe="normalized", use_spectral_gap=True
 
 def heat_kernel(laplacian, timestep, measure):
     """compute matrix exponential on a measure"""
+    # return expm(-timestep * laplacian).dot(measure)
     return expm_multiply(-timestep * laplacian, measure)
 
 
@@ -175,7 +184,7 @@ def compute_node_trajectories(laplacian, initial_measure, times, disable_tqdm=Fa
     ]
     for i in tqdm(range(len(times) - 1), disable=disable_tqdm):
         node_trajectories.append(
-            heat_kernel(times[i + 1] - times[i], laplacian, node_trajectories[-1])
+            heat_kernel(laplacian, times[i + 1] - times[i], node_trajectories[-1])
         )
     return np.array(node_trajectories)
 
@@ -228,11 +237,15 @@ def extract_relative_dimensions(times, node_trajectories, initial_measure, spect
     return relative_dimensions, peak_times, peak_amplitudes, diffusion_coefficient
 
 
-def get_initial_measure(graph, nodes, total_degree):
+def get_initial_measure(graph, nodes, total_degree, directed=False):
     """create an measure with the correct mass from a list of nodes"""
     measure = np.zeros(len(graph))
-    measure[nodes] = [
-        total_degree / (len(graph) * graph.degree(node, weight="weight")) / len(nodes)
-        for node in nodes
-    ]
+    if directed:
+        for node in nodes:
+            d = graph.out_degree(node, weight="weight")
+            measure[node] = total_degree / (len(graph) * d) / len(nodes)
+    else:
+        for node in nodes:
+            d = graph.degree(node, weight="weight")
+            measure[node] = total_degree / (len(graph) * d) / len(nodes)
     return measure
